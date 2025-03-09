6be74ad35bdc40d149a1c509ccfeaf97ac4dde0e
Katrusik1 <stolbova8621@gmail.com>
2025-03-09 22:59:23 +0700

    сделала поиск по названию у пользователей и счетов

--- src/widgets/usersBills/BillsList.tsx
+++ src/widgets/usersBills/BillsList.tsx
@@ -3,26 +3,36 @@ import { useParams } from "react-router-dom";
 import { useGetUsersBillsQuery } from "../../shared/lib/api/clients";
 import { BillCard } from "../../entities/billCard";
 
+export const UsersBills = ({ searchQuery }:{searchQuery:string}) => {
+    const { id } = useParams();
+    console.log(searchQuery)
+    const { data, isLoading } = useGetUsersBillsQuery(id!);
 
-export const UsersBills = () => {
-    const {id} = useParams()
-    const {data, isLoading}= useGetUsersBillsQuery(id!)
-    if(isLoading){
-        return <h1>Загрузка</h1>
+    if (isLoading) {
+        return <h1>Загрузка...</h1>;
     }
-    console.log(data)
-  return (
-    <>
-        {data?.length!==0&& data !== undefined ?
-        <SimpleGrid cols={2} spacing="md" style={{width:"100%", marginTop:"2vh"}} verticalSpacing="xl">
-            {data?.map(elem=>{
-                return(
-                    <BillCard name={elem.name} type={elem.type} amount={elem.amount} status={elem.status} key={elem.id} id={elem.id}/>
-                )}
-            )}
-        </SimpleGrid>:
-        <h1>У данного пользователя еще нет счетов</h1>
-        }
-    </>
-  );
+
+    const filteredBills = data?.filter((bill) =>
+        bill.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    return (
+        <>
+            {filteredBills?.length ? (
+                <SimpleGrid cols={2} spacing="md" style={{ width: "100%", marginTop: "2vh" }} verticalSpacing="xl">
+                    {filteredBills.map((elem) => (
+                        <BillCard
+                            key={elem.id}
+                            id={elem.id}
+                            name={elem.name}
+                            type={elem.type}
+                            amount={elem.amount}
+                            status={elem.status}
+                        />
+                    ))}
+                </SimpleGrid>
+            ) : (<>{searchQuery.length?<h1>У данного пользователя нет счетов с таким названием</h1>:<h1>У данного пользователя еще нет счетов</h1>}
+            </>)}
+        </>
+    );
 };
